98ac98bec4d17d9ff23decd63c74004f20c19d1e
WirelessSocks <romka116rus@icloud.com>
2023-10-07 17:23:22 +0300

    added TodoItem

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import Todoitem from "./Todoitem";
+
+
+const todos = [
+{
+    _id: 'asdasd1',
+    title: "Finish marathon",
+    isComplited: false
+},
+{
+    _id: 'asdsdfs24',
+    title: "Read the next chapter of the book",
+    isComplited: false
+},
+{
+    _id: 'ahgfhfg42',
+    title: "Do the math for the next monday",
+    isComplited: false
+}
+]
+
 
 const Home = () => {
 
     return (
-        <div>
-            Home
+        <div className="bg-gray-900 h-screen text-white">
+            {todos.map(todo => (
+                <Todoitem key={todo._id} todo= {todo}/>
+            ))}
         </div>
     )
 }
---
+++ src/components/screens/Home/Todoitem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Todoitem = ({todo}) => {
+    return (
+        <div>
+            {todo.title}
+        </div>
+    );
+}
+
+export default Todoitem;
