62816b072822a2a59adace9f1c48cdbd4da02b15
WirelessSocks <romka116rus@icloud.com>
2023-10-10 00:15:35 +0300

    added delete button

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -25,18 +25,23 @@ const Home = () => {
 
     const [todos, setTodos] = useState(data)
 
-    const changeTodo = (id) => {
+    const changeTodo = id => {
         const copy = [...todos]
         const current = copy.find(t => t._id === id)
         current.isCompleted = !current.isCompleted
         setTodos(copy)
     }
 
+    const removeTodo = id => setTodos([...todos].filter(t => t._id !== id))
+    
+
     return (
         <div className="text-white w-4/5 mx-auto">
             <h1 className="text-2xl font-bold text-center mb-8">Todo for junior</h1>
             {todos.map(todo => (
-                <Todoitem key={todo._id} todo= {todo} changeTodo= {changeTodo}/>
+                <Todoitem key={todo._id} todo= {todo} changeTodo= {changeTodo}
+                    removeTodo= {removeTodo}
+                />
             ))}
         </div>
     )
--- src/components/screens/Home/item/Todoitem.jsx
+++ src/components/screens/Home/item/Todoitem.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import Check from './Check';
+import {BsTrash} from 'react-icons/bs';
 
 
-const Todoitem = ({ todo, changeTodo }) => {
+const Todoitem = ({ todo, changeTodo, removeTodo }) => {
     return (
-        <button className='flex items-center mb-2 rounded-2xl bg-gray-800 p-5 w-full' 
-            onClick={() => changeTodo(todo._id)}
+        <div className='flex items-center justify-between mb-2 rounded-2xl bg-gray-800 p-5 w-full' 
+            
         >
-            <Check isCompleted={todo.isCompleted}/>
-            {todo.title}
-        </button>
+            <button className='flex items-center'
+                onClick={() => changeTodo(todo._id)}
+            >
+                <Check isCompleted={todo.isCompleted}/>
+                <span
+                    className={`${todo.isCompleted ? 'line-through' : ''}`}
+                >
+                    {todo.title}
+                </span>
+            </button>
+            <button onClick={() => removeTodo(todo._id)}>
+                <BsTrash 
+                    size={22}
+                    className='text-gray-600 hover:text-red-700
+                    transition-colors ease-in-out duration-300' 
+                />
+            </button>
+        </div>
     );
 }
 
